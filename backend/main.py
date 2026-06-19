916e34a3f07975907ef321927e11b6f05f1874a0
tannmayygupta <tanmayg460@gmail.com>
2026-06-20 00:39:58 +0530

    Riyaz Entry - C:\Users\tanma\riyazzz\notes\2026\06\20.md

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,21 @@ from fastapi import FastAPI
 from git_service import commit_and_push
 from models import EntryRequest
 from markdown_service import save_entry
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+
+    allow_origins=["*"],
+
+    allow_credentials=True,
+
+    allow_methods=["*"],
+
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
